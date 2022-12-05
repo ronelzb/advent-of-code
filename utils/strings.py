0bd5887bfa18fb6e3b214981056570eca5faeb41
Ronel Zapata <ronelzb@gmail.com>
2022-12-05 11:29:29 -0600

    add custom trim to parse text checking if input is docstring

--- utils/strings.py
+++ utils/strings.py
@@ -1,5 +1,5 @@
+import sys
 from functools import wraps
-from textwrap import dedent
 from typing import Callable, TypeVar
 
 T = TypeVar("T")
@@ -11,7 +11,7 @@ def parse_text_as_list(fn) -> Callable[[list[str]], T]:
     """
     @wraps(fn)
     def wrapper(*args):
-        text = dedent(args[0]).strip()
+        text = trim(args[0])
         input_list = [line for line in text.split("\n")]
         return fn(input_list)
 
@@ -25,8 +25,46 @@ def parse_text_as_paragraphs(fn) -> Callable[[list[list[str]]], T]:
     """
     @wraps(fn)
     def wrapper(*args):
-        text = dedent(args[0]).strip()
+        text = trim(args[0])
         paragraphs = [[s for s in p.splitlines()] for p in text.split("\n\n")]
         return fn(paragraphs)
 
     return wrapper
+
+
+def trim(raw: str) -> str:
+    if not raw:
+        return ""
+
+    # Convert tabs to spaces (following the normal Python rules)
+    # and split into a list of lines
+    lines = raw.expandtabs().splitlines()
+
+    # check if first line is empty (following docstring postprocessing PEP 257)
+    first_line_is_empty = not lines[0].strip()
+
+    # Determine minimum indentation (first line doesn't count if empty)
+    start_line = 1 if first_line_is_empty else 0
+
+    indent = sys.maxsize
+    for line in lines[start_line:]:
+        stripped = line.lstrip()
+        if stripped:
+            indent = min(indent, len(line) - len(stripped))
+
+    # Remove indentation
+    first_line = lines[0].strip() if first_line_is_empty else lines[0][indent:]
+
+    trimmed = [first_line]
+    if indent < sys.maxsize:
+        for line in lines[1:]:
+            trimmed.append(line[indent:])
+
+    # Strip off trailing and leading blank lines:
+    while trimmed and not trimmed[-1]:
+        trimmed.pop()
+    while trimmed and not trimmed[0]:
+        trimmed.pop(0)
+
+    # Return a single string:
+    return "\n".join(trimmed)
